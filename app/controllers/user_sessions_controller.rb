a2effc4e4a12375be3f8f25802c15cdeeba9ca6d
dani <danigb@gmail.com>
2009-10-07 17:53:54 +0200

    inherit resources

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,37 +1,15 @@
 class UserSessionsController < ApplicationController
-  before_filter :require_user, :only => [:destroy]
-
-  def new
-    @user_session = UserSession.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user_session }
-    end
-  end
+  inherit_resources
+  respond_to :html
+  actions :new, :create, :destroy
 
   def create
-    @user_session = UserSession.new(params[:user_session])
-
-    respond_to do |format|
-      if @user_session.save
-        flash[:notice] = 'UserSession was successfully created.'
-        format.html { redirect_to_stored_or account_path }
-        format.xml  { render :xml => @user_session, :status => :created, :location => @user_session }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user_session.errors, :status => :unprocessable_entity }
-      end
+    create! do |format|
+      format.html { redirect_to_stored_or account_path}
     end
   end
 
   def destroy
-    @user_session = UserSession.find(params[:id])
-    @user_session.destroy
-
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.xml  { head :ok }
-    end
+    destroy! { login_url }
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem "graysky-translator", :lib => "translator"
-  #config.gem "josevalim-inherited_resources"
+  config.gem "josevalim-inherited_resources", :lib => 'inherited_resources'
   
   config.time_zone = 'UTC'
   config.i18n.default_locale = :es
